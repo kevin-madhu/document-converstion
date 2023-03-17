4cd095d9a3dabb3f7932b109d8e98415fc1f0295
Kevin Madhu <kevin.madhu@gmail.com>
2023-03-17 16:20:01 +0300

    Initial commit.

---
+++ src/main/java/com/ediweb/interview/documentconverstion/DocumentConverstionApplication.java
@@ -0,0 +1,13 @@
+package com.ediweb.interview.documentconverstion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentConverstionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentConverstionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ediweb/interview/documentconverstion/DocumentConverstionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ediweb.interview.documentconverstion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentConverstionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
