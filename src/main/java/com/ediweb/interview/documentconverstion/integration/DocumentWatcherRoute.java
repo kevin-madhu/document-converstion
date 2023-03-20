6493715ed1808bd2705321361f102f5f358c5c43
Kevin Madhu <kevin.madhu@gmail.com>
2023-03-20 08:54:58 +0300

    Adds loggin levels for camel logs.

--- src/main/java/com/ediweb/interview/documentconverstion/integration/DocumentWatcherRoute.java
+++ src/main/java/com/ediweb/interview/documentconverstion/integration/DocumentWatcherRoute.java
@@ -2,6 +2,7 @@ package com.ediweb.interview.documentconverstion.integration;
 
 import com.ediweb.interview.documentconverstion.config.misc.ApplicationProperties;
 import com.ediweb.interview.documentconverstion.domain.enumeration.CamelExchangeProperty;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
@@ -18,20 +19,26 @@ public class DocumentWatcherRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+        final String ORIGINAL_DOCUMENT_CREATED_MESSAGE = "Original Document created with id ${exchange.getProperty("
+                + CamelExchangeProperty.ORIGINAL_DOCUMENT_ID + ")}";
+        final String XSLT_CONVERSION_SUCCESS_MESSAGE = "Converted document as per XSLT";
+        final String XML_TRANSFORMER_EXCEPTION_MESSAGE = "Failed conversion: Conversion error occurred on file ${header.CamelFileName}.";
+        final String XSLT_CONVERSION_FAILED_MESSAGE = "Exception: ${exception.message}.";
+
 
         from("file:" + applicationProperties.getCamel().getWatchFileDirectory() + "?delete=true")
                 .convertBodyTo(String.class)
                 .bean(OriginalDocumentInputProcessor.class)
-                .log("Original Document created with id ${exchange.getProperty(" + CamelExchangeProperty.ORIGINAL_DOCUMENT_ID + ")}")
+                .log(LoggingLevel.INFO, ORIGINAL_DOCUMENT_CREATED_MESSAGE)
                 .doTry()
                     .to("xslt:" + applicationProperties.getCamel().getXsltPath())
-                    .log("Converted document as per XSLT")
+                    .log(LoggingLevel.INFO, XSLT_CONVERSION_SUCCESS_MESSAGE)
                 .doCatch(TransformerException.class)
-                    .log("Failed conversion: Conversion error occurred on file ${header.CamelFileName}. Removing.")
+                    .log(LoggingLevel.ERROR, XML_TRANSFORMER_EXCEPTION_MESSAGE)
                     .bean(OriginalDocumentInputProcessor.class, "processingError")
                     .stop()
                 .doCatch(Exception.class)
-                    .log("Exception: ${exception.message}. Removing.")
+                    .log(LoggingLevel.ERROR, XSLT_CONVERSION_FAILED_MESSAGE)
                     .bean(OriginalDocumentInputProcessor.class, "processingError")
                 .stop()
                 .end()
